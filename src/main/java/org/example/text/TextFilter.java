9eaa5ea7a3ae5101fdc55cf256c253cc0b04c4be
puma <92054042+melnikova-maria@users.noreply.github.com>
2025-05-11 21:10:39 +0300

    Update TextFilter.java

--- src/main/java/org/example/text/TextFilter.java
+++ src/main/java/org/example/text/TextFilter.java
@@ -4,8 +4,6 @@ package org.example.text;
  * Allows to filter content from a text to be ignored for the n-gram analysis.
  *
  * <p>Implementations must be immutable and stateless.</p>
- *
- * @author Fabian Kessler
  */
 public interface TextFilter {
 
